61a09c8398e3514f07484f1eadb45c12af0a7e7b
alimmiftahul <alimmiftaul08@gmail.com>
2020-10-13 13:14:06 +0700

    menambahkan video untuk bahan belajar opencv

---
+++ capture-image-video-cam.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np
+
+##---------------capture image---------##
+# img = cv2.imread("Picture/Football.jpg")
+# cv2.imshow("output", img)
+# cv2.waitKey(0)
+
+##--------------video capture----------##
+cap = cv2.VideoCapture("")
