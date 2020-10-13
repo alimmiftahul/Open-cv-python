e605e187b3828101d058e6e402dd11af86e31e6d
alimmiftahul <alimmiftaul08@gmail.com>
2020-10-13 13:25:45 +0700

    menambahkan coding screen camera, gambar, dan video pada capture-image-vide-camera.py

--- capture-image-video-cam.py
+++ capture-image-video-cam.py
@@ -7,4 +7,22 @@ import numpy as np
 # cv2.waitKey(0)
 
 ##--------------video capture----------##
-cap = cv2.VideoCapture("")
+# cap = cv2.VideoCapture("Video/20secondFootball.mp4")
+# while True :
+#     success, img =cap.read()
+#     cv2.imshow("Video",img)
+#     if cv2.waitKey(21) & 0Xff ==ord('q'):
+#         break
+#
+##------------screen camera--------------##
+## index 0 pada videocapture = 0 = camera 1 yang dipakai. 1 = camera 2
+cap = cv2.VideoCapture(0)
+##mengatur atau resize frame dengan set, set id 3 = width,4 = height, 10 =brigthness
+cap.set(3,480)
+cap.set(4,480)
+cap.set(10,20)
+while True :
+    success, img =cap.read()
+    cv2.imshow("Camera", img)
+    if cv2.waitKey(1) & 0xFF ==ord('q') :
+        break
